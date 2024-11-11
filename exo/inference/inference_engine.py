1a2179cdef241b1bcd9f2aa67fc207a04e1749aa
tangwei <tangwei@ubuntu2404desktop.office>
2024-11-11 19:16:10 +0800

    init

---
+++ exo/inference/inference_engine.py
@@ -0,0 +1,36 @@
+import numpy as np
+import os
+from exo.helpers import DEBUG  # Make sure to import DEBUG
+
+from typing import Tuple, Optional
+from abc import ABC, abstractmethod
+from .shard import Shard
+
+
+class InferenceEngine(ABC):
+  @abstractmethod
+  async def infer_prompt(self, request_id: str, shard: Shard, prompt: str, image_str: Optional[str] = None, inference_state: Optional[str] = None) -> Tuple[np.ndarray, str, bool]:
+    pass
+
+  @abstractmethod
+  async def infer_tensor(self, request_id: str, shard: Shard, input_data: np.ndarray, inference_state: Optional[str] = None) -> Tuple[np.ndarray, str, bool]:
+    pass
+
+
+def get_inference_engine(inference_engine_name: str, shard_downloader: 'ShardDownloader'):
+  if DEBUG >= 2:
+    print(f"get_inference_engine called with: {inference_engine_name}")
+  if inference_engine_name == "mlx":
+    from exo.inference.mlx.sharded_inference_engine import MLXDynamicShardInferenceEngine
+
+    return MLXDynamicShardInferenceEngine(shard_downloader)
+  elif inference_engine_name == "tinygrad":
+    from exo.inference.tinygrad.inference import TinygradDynamicShardInferenceEngine
+    import tinygrad.helpers
+    tinygrad.helpers.DEBUG.value = int(os.getenv("TINYGRAD_DEBUG", default="0"))
+
+    return TinygradDynamicShardInferenceEngine(shard_downloader)
+  elif inference_engine_name == "dummy":
+    from exo.inference.dummy_inference_engine import DummyInferenceEngine
+    return DummyInferenceEngine()
+  raise ValueError(f"Unsupported inference engine: {inference_engine_name}")
